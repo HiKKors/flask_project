8706265ca57e8dd51efdfc3cde269e1b0c83a279
HiKKors <nk041496@gmail.com>
2024-05-11 10:59:28 +0400

    разделение метода поиска

--- Controllers/calendarDayController.py
+++ Controllers/calendarDayController.py
@@ -1,24 +1,23 @@
+from application import app
+
 from flask import Flask, jsonify, request
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Resource, reqparse
 
 from Models.CalendarDays import CalendarDays
 from Services.calendarDaysService import CalendarDayService
-
-app = Flask(__name__)
-api = Api()
 
 _calendarDayService = CalendarDayService()
 
 class CalendarDayController(Resource):
     @staticmethod
     @app.route('/cds/v1/calendarDays', methods=['GET'])
+    def get_calendarDays():
+        return jsonify({'calendar_days': _calendarDayService.findAllCalendarDays()})
+        
     @app.route('/cds/v1/calendarDays/<int:calendar_day_id>', methods=['GET'])
-    def get_calendarDays(calendar_day_id = None):
-        if calendar_day_id != None:
-            calendar_day = _calendarDayService.findCalendarDay(calendar_day_id)
-            return jsonify(calendar_day.serialize())
-        else:
-            return jsonify({'calendar_days': _calendarDayService.findAllCalendarDays()})
+    def get_calendarDay(calendar_day_id = None):
+        calendar_day = _calendarDayService.findCalendarDay(calendar_day_id)
+        return jsonify(calendar_day.serialize())
         
     @staticmethod
     @app.route('/cds/v1/calendarDays/<int:id>', methods=['DELETE'])
@@ -34,7 +33,3 @@ class CalendarDayController(Resource):
     @app.route('/cds/v1/calendarDays/<int:id>', methods=['PUT'])
     def update_calendarDay(id):
         pass
-
-    def start(self):
-        api.init_app(app)
-        return app
